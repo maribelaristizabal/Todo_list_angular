13f51db0af93249a77e7b0a542c30fa008852170
maribelaristizabal <maribelaristizabal079@gmail.com>
2023-08-09 16:56:50 -0500

    repository all ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Item } from './item';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'todo';
+  title = "todo";
+  
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "go out to the supermarket", done: true },
+    { description: "do exercise", done: false },
+    { description: "laundry", done: false },
+    { description: "llaundry", done: false },
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+      
+    );
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+  
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+  
+
 }
+
+
